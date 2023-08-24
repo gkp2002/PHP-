8f1deccb435eb568491d3bd5ef277b7212ae22be
gkp2002 <gajanank015@gmail.com>
2023-08-24 14:49:03 +0530

    Right

--- input.php
+++ input.php
@@ -6,14 +6,17 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="" method="post">
+        <label for="">Enter  first number&nbsp;&nbsp;&emsp14;&emsp13;</label>
+        <input type="text" name="num1"><br><br>
+        <label for="">Enter  second number</label>
+        <input type="text" name="num2"><br><br>
+        <input type="submit" >
+    </form>
     <?php
-       if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name= $_POST["n1"];
-        $email = $_POST["n2"];
-        echo "Name ".$name."<br>";
-        echo "Email ".$email."<br>";
-       }
-    
-    ?>
+       $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+       echo "<h1>",$num1+$num2,"</h1>";
+       ?>
 </body>
 </html>
